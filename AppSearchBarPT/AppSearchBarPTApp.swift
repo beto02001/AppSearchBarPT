037b2403665b3f3ba8780c490fe6a70c8d6de543
beto02001 <beto020017@gmail.com>
2025-12-03 12:03:55 -0600

    Initial Commit

---
+++ AppSearchBarPT/AppSearchBarPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSearchBarPTApp.swift
+//  AppSearchBarPT
+//
+//  Created by Luis Humberto Martinez Echegaray on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AppSearchBarPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
